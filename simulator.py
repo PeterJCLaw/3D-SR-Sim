d3af7c40589aa2b4a7750ae1acb79eddb8a5acd6
Peter Law <PeterJCLaw@gmail.com>
2014-08-15 22:45:49 +0100

    Whitespace changes before moving code.
    Otherwise my editor is likely to cause extra changes we don't care
    about.

--- simulator.py
+++ simulator.py
@@ -1,24 +1,24 @@
 from objects import *
 from variables import *
-import thread,time
+import thread, time
 import random
 '''
 #################
 Usercode Function
 #################
-'''    
+'''
 
-def SR_filter(listname,markertype):
-    temporary_list=[]
+def SR_filter(listname, markertype):
+    temporary_list = []
     for l in listname:
-        if l.marker_type== markertype:
+        if l.marker_type == markertype:
             temporary_list.append(l)
-    listname=temporary_list
+    listname = temporary_list
     return listname
 
 
 def distance_orderer(listname):
-    listname=sorted(listname, key=lambda Marker:Marker.distance)
+    listname = sorted(listname, key = lambda Marker: Marker.distance)
     return listname
 
 
@@ -35,18 +35,18 @@ def usercode0():
         markers = R0.see()
         markers = SR_filter(markers, 'TOKEN')
 
-            
-        if len(markers)>0:
+
+        if len(markers) > 0:
             angle = markers[0].bearing.y
             print markers[0].marker_type
             print angle
-            if angle >10 and angle <30:
+            if angle > 10 and angle < 30:
                 R0.motors[0].speed = -40
                 R0.motors[1].speed = 40
             elif angle < -10 and angle > -30:
                 R0.motors[0].speed = 40
                 R0.motors[1].speed = -40
-            elif angle <10 and angle >-10:
+            elif angle < 10 and angle > -10:
                 R0.motors[0].speed = 50
                 R0.motors[1].speed = 50
         else:
@@ -59,87 +59,90 @@ def usercode1():
         markers = R1.see()
         markers = SR_filter(markers, 'TOKEN')
 
-            
-        if len(markers)>0:
+
+        if len(markers) > 0:
             angle = markers[0].bearing.y
             print markers[0].marker_type
             print angle
-            if angle >10 and angle <30:
+            if angle > 10 and angle < 30:
                 R1.motors[0].speed = -40
                 R1.motors[1].speed = 40
             elif angle < -10 and angle > -30:
                 R1.motors[0].speed = 40
                 R1.motors[1].speed = -40
-            elif angle <10 and angle >-10:
+            elif angle < 10 and angle > -10:
                 R1.motors[0].speed = 50
                 R1.motors[1].speed = 50
         else:
             R1.motors[0].speed = -100
             R1.motors[1].speed = 100
+
 def usercode2():
     time.sleep(10)
     while True:
         markers = R2.see()
         markers = SR_filter(markers, 'TOKEN')
 
-            
-        if len(markers)>0:
+        if len(markers) > 0:
             angle = markers[0].bearing.y
             print markers[0].marker_type
             print angle
-            if angle >10 and angle <30:
+            if angle > 10 and angle < 30:
                 R2.motors[0].speed = -40
                 R2.motors[1].speed = 40
             elif angle < -10 and angle > -30:
                 R2.motors[0].speed = 40
                 R2.motors[1].speed = -40
-            elif angle <10 and angle >-10:
+            elif angle < 10 and angle > -10:
                 R2.motors[0].speed = 50
                 R2.motors[1].speed = 50
         else:
             R2.motors[0].speed = -100
             R2.motors[1].speed = 100
-            
+
 def usercode3():
     time.sleep(10)
     while True:
         markers = R3.see()
         markers = SR_filter(markers, 'TOKEN')
-            
-        if len(markers)>0:
+
+        if len(markers) > 0:
             angle = markers[0].bearing.y
             print markers[0].marker_type
             print angle
-            if angle >10 and angle <30:
+            if angle > 10 and angle < 30:
                 R3.motors[0].speed = -40
                 R3.motors[1].speed = 40
             elif angle < -10 and angle > -30:
                 R3.motors[0].speed = 40
                 R3.motors[1].speed = -40
-            elif angle <10 and angle >-10:
+            elif angle < 10 and angle > -10:
                 R3.motors[0].speed = 50
                 R3.motors[1].speed = 50
         else:
             R3.motors[0].speed = -100
             R3.motors[1].speed = 100
 
-a=Arena()
-populate_walls(7,7)
-for i in xrange(40,NUMBER_OF_TOKENS+40):
+a = Arena()
+populate_walls(7, 7)
+for i in xrange(40, NUMBER_OF_TOKENS+40):
     token_list.append(Token(i))
+
 robotlist = []
-R0=Robot(0)
-R1=Robot(1)
-R2=Robot(2)
-R3=Robot(3)
+R0 = Robot(0)
+R1 = Robot(1)
+R2 = Robot(2)
+R3 = Robot(3)
+
 robotlist.append(R0)
 robotlist.append(R1)
 robotlist.append(R2)
 robotlist.append(R3)
-thread.start_new_thread(usercode0,())
-thread.start_new_thread(usercode1,())
-thread.start_new_thread(usercode2,())
-thread.start_new_thread(usercode3,())
+
+thread.start_new_thread(usercode0, ())
+thread.start_new_thread(usercode1, ())
+thread.start_new_thread(usercode2, ())
+thread.start_new_thread(usercode3, ())
 
 while True:
     n = 2
@@ -147,7 +150,7 @@ while True:
         # Simulation step
         world.step(dt/n)
     for robot in robotlist:
-        robot.update() 
+        robot.update()
     for token in token_list:
         token.update()
     rate(RATE)
